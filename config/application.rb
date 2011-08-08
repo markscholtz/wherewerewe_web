3d9680d994a4d34665b2d2be008f3e2c76c173f1
Mark Scholtz <markscholtz@gmail.com>
2011-08-08 18:32:50 +0200

    Remove helpers and view specs from generators

--- config/application.rb
+++ config/application.rb
@@ -30,6 +30,11 @@ module Wherewerewe
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.generators do |g|
+      g.helper false
+      g.view_specs false
+    end
+
     # JavaScript files you want as :defaults (application.js is always included).
     config.action_view.javascript_expansions[:defaults] = %w()
 
