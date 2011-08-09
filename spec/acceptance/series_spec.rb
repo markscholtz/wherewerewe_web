156cb9eae5cf73dda53601751b286b0499fa1e8f
Mark Scholtz <markscholtz@gmail.com>
2011-08-09 17:08:47 +0200

    Add first capybara acceptance test

--- spec/acceptance/series_spec.rb
+++ spec/acceptance/series_spec.rb
@@ -1,10 +1,17 @@
 require 'spec_helper'
 
-describe "Series" do
-  describe "GET /series" do
-    it "should display all series available for watching" do
-      visit series_index_path
-      page.should have_content("Find me")
-    end
+feature "Series", %q{
+  In order to view the series that I am currently watching
+  As a user
+  I want to view the series page
+} do
+
+  background do
+    @series = Factory(:series)
+  end
+
+  scenario "Series index" do
+    visit series_index_path
+    page.should have_content("Find me")
   end
 end
