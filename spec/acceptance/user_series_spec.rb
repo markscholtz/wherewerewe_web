a2499f7c93c8ee8de869cd4c8d146cf904dcacdf
Mark Scholtz <markscholtz@gmail.com>
2011-08-21 00:51:55 +0200

    Improve user series acceptance test (still buggy)

--- spec/acceptance/user_series_spec.rb
+++ spec/acceptance/user_series_spec.rb
@@ -1,23 +1,34 @@
 require 'spec_helper'
 
-feature 'Series', %q{
+feature 'User Series', %q{
   In order to view the series that I am currently watching
   As a user
-  I want to view the series page
+  I want to view my series page
 } do
 
   background do
+    @user = Factory(:user)
     @series1 = Factory(:series, :name => 'Fringe', :overview => 'Supernatural stuff')
     @series2 = Factory(:series, :name => 'Boston Legal', :overview => 'Some funny lawyers')
+    @episode1 = Factory(:episode, :series => @series1, :name => "Episode 1")
+    @episode2 = Factory(:episode, :series => @series1, :name => "Episode 2")
+    @episode3 = Factory(:episode, :series => @series2, :name => "Episode 3")
+    @viewing1 = Factory(:viewing, :user => @user, :episode => @episode1, :series => @series1, :viewed_at => 3.days.ago)
+    @viewing2 = Factory(:viewing, :user => @user, :episode => @episode2, :series => @series1, :viewed_at => 1.days.ago)
+    @viewing3 = Factory(:viewing, :user => @user, :episode => @episode3, :series => @series1, :viewed_at => 2.days.ago)
+    @viewing4 = Factory(:viewing, :user => @user, :episode => @episode3, :series => @series2)
   end
 
   scenario 'Viewing the "currently watching" series' do
     visit series_index_path
 
-    page.should have_content @series1.name
-    page.should have_content @series1.overview
+    #save_and_open_page
+    page.should have_content @user.series[0].name
+    page.should have_content @user.series[0].overview
+    page.should have_content Viewing.last_viewed(user_id: @user.id, series_id: @user.series[0].id).first.episode.name
 
-    page.should have_content @series2.name
-    page.should have_content @series2.overview
+    page.should have_content @user.series[1].name
+    page.should have_content @user.series[1].overview
+    page.should_not have_content Viewing.last_viewed(user_id: @user.id, series_id: @user.series[1].id).first.episode.name
   end
 end
