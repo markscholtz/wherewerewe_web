1287795bbe4fdb66656d550d16d6f8a0be48872e
Mark Scholtz <markscholtz@gmail.com>
2012-04-16 18:29:33 +0200

    Rack hotfix
    Remove UTF-8 warning from Rack. Details here
    http://stackoverflow.com/a/7189698/481677.

---
+++ config/initializers/rack_hotfix.rb
@@ -0,0 +1,16 @@
+# TODO: Can be removed after updating to rack 1.3.0
+module Rack
+  module Utils
+    def escape(s)
+      CGI.escape(s.to_s)
+    end
+
+    def unescape(s)
+      CGI.unescape(s)
+    end
+
+    def self.hotfixed
+      true
+    end
+  end
+end
