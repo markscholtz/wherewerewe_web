93c6488c65fde76bb4d623903ec3d031093f379d
Mark Scholtz <markscholtz@gmail.com>
2012-05-22 23:52:49 +0200

    Ensure seasons and episodes are ordered correctly

--- spec/acceptance/user/view_series_details_spec.rb
+++ spec/acceptance/user/view_series_details_spec.rb
@@ -10,8 +10,28 @@ feature 'View series details feature', %q{
     @user = FactoryGirl.create(:user)
     @boston_legal = FactoryGirl.create(:series, :name => 'Boston Legal', :overview => 'Some funny lawyers')
     @bl_s1 = FactoryGirl.create(:season, :number => 1, :series => @boston_legal)
-    @bl_ep1 = FactoryGirl.create(:episode, :series => @boston_legal, :season => @bl_s1, :name => "Boston Legal - Episode 1")
-    @viewing1 = FactoryGirl.create(:viewing, :user => @user, :episode => @bl_ep1, :season => @bl_s1, :series => @boston_legal, :viewed_at => 3.days.ago)
+    @bl_s2 = FactoryGirl.create(:season, :number => 2, :series => @boston_legal)
+    @bl_ep2 = FactoryGirl.create(:episode,
+                                 :series => @boston_legal,
+                                 :season => @bl_s1,
+                                 :name => "Boston Legal - Episode 2",
+                                 :number => 2)
+    @bl_ep1 = FactoryGirl.create(:episode,
+                                 :series => @boston_legal,
+                                 :season => @bl_s1,
+                                 :name => "Boston Legal - Episode 1",
+                                 :number => 1)
+    @bl_ep3 = FactoryGirl.create(:episode,
+                                 :series => @boston_legal,
+                                 :season => @bl_s2,
+                                 :name => "Boston Legal - Episode 1, Season 2",
+                                 :number => 1)
+    @viewing1 = FactoryGirl.create(:viewing,
+                                   :user => @user,
+                                   :episode => @bl_ep1,
+                                   :season => @bl_s1,
+                                   :series => @boston_legal,
+                                   :viewed_at => 3.days.ago)
   end
 
   scenario 'Navigating to the series details page' do
@@ -23,7 +43,17 @@ feature 'View series details feature', %q{
   scenario 'Inspecting the series details page' do
     visit series_path(@boston_legal)
     page.should have_content @boston_legal.name
-    page.should have_content "Season #{@bl_s1.number}"
-    page.should have_content @bl_ep1.name
+    within(:xpath, '//section[@id="season"][1]') do
+      page.should have_content "Season #{@bl_s1.number}"
+      within(:xpath, ".//ol//li[1]") do
+        page.should have_content @bl_ep1.name
+      end
+      within(:xpath, ".//ol//li[2]") do
+        page.should have_content @bl_ep2.name
+      end
+    end
+    within(:xpath, '//section[@id="season"][2]') do
+      page.should have_content "Season #{@bl_s2.number}"
+    end
   end
 end
