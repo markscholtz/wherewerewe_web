0446bbc91a2e1edb999eabd81d832acaf316aef9
Mark Scholtz <markscholtz@gmail.com>
2012-05-24 20:34:03 +0200

    Refactor watching an episode spec

--- spec/acceptance/user/watch_viewing_spec.rb
+++ spec/acceptance/user/watch_viewing_spec.rb
@@ -7,19 +7,32 @@ feature 'User watch viewing feature', %q{
 } do
 
   background do
-    @user   = FactoryGirl.create(:user)
-    # @fringe = FactoryGirl.create(:series, :name => 'Fringe', :overview => 'Supernatural stuff')
-    # @f_s1   = FactoryGirl.create(:season, :number => 1, :series => @fringe)
-    # @f_ep1  = FactoryGirl.create(:episode, :series => @fringe, :season => @f_s1, :name => "Fringe - Episode 1")
-    @v1 = FactoryGirl.create(:viewing, :user => @user)
+    @user = FactoryGirl.create(:user)
+    @episode = FactoryGirl.create(:episode, :name => 'Cool episode')
+    @v1 = FactoryGirl.create(:viewing, :user => @user, :episode => @episode)
   end
 
-  scenario 'Watching an episode' do
+  scenario 'watching the last unwatched episode' do
     visit viewings_path
-    # save_and_open_page
+
+    within('#last_viewing') do
+      page.should have_content 'No previously watched episodes'
+    end
+    within('#next_viewing') do
+      page.should have_button 'Watch'
+      page.should have_content 'Cool episode'
+    end
+
     click_button 'Watch'
-    page.should_not have_selector 'input'
-    page.should have_content 'No upcoming episodes'
+
+    within('#last_viewing') do
+      page.should have_content 'Cool episode'
+    end
+    within('#next_viewing') do
+      page.should_not have_button 'Watch'
+      page.should have_content 'No upcoming episodes'
+    end
+
     current_path.should == viewings_path
   end
 end
