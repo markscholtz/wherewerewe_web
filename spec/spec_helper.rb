259b1f442fd678b0d342db98fbcd0d08a1808973
Mark Scholtz <markscholtz@gmail.com>
2012-06-27 16:40:48 +0200

    Add authentication helper methods for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,10 @@ require 'rspec/rails'
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
+
+  config.include AcceptanceHelpers, :type => :request
+  config.include ControllerHelpers, :type => :controller
+
   # == Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
---
+++ spec/support/authentication.rb
@@ -0,0 +1,22 @@
+module AcceptanceHelpers
+
+  def log_in(user)
+    visit log_in_path
+    fill_in 'email', :with => user.email
+    fill_in 'password', :with => user.password
+    click_button 'log in'
+  end
+
+end
+
+module ControllerHelpers
+
+  def current_user
+    User.find session[:user_id]
+  end
+
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+end
