f59b1d688d6698e0613a4a345de45a813f85218a
Mark Scholtz <markscholtz@gmail.com>
2012-08-18 19:48:37 +0200

    Automatically load and authorize Progressions

--- app/controllers/progressions_controller.rb
+++ app/controllers/progressions_controller.rb
@@ -1,9 +1,9 @@
 class ProgressionsController < ApplicationController
+  load_and_authorize_resource
+
   def index
     unless current_user
       raise CanCan::AccessDenied
     end
-
-    @progressions = current_user.progressions if current_user
   end
 end
