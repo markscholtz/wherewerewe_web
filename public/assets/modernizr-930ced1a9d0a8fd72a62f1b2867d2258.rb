fa0b9c33b3bbffb2b5217b3249db4a21e2e01725
Mark Scholtz <markscholtz@gmail.com>
2012-08-28 00:07:09 +0200

    Add precompiled assets

---
+++ public/assets/modernizr-930ced1a9d0a8fd72a62f1b2867d2258.rb
@@ -0,0 +1,18 @@
+module Modernizr
+  VERSION = "2.6.1"
+
+  PATH = File.expand_path("..", __FILE__)
+
+  def self.path
+    PATH
+  end
+
+  # Rails "Magic"
+  if defined? ::Rails::Railtie
+    class Railtie < ::Rails::Railtie
+      initializer "modernizr" do |app|
+        app.config.assets.paths << Modernizr.path
+      end
+    end
+  end
+end
