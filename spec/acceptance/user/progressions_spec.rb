048e0f0aebb1ee73d03f511fbe20bb9e210034e2
Mark Scholtz <markscholtz@gmail.com>
2012-09-03 20:51:38 +0200

    Refactor test

--- spec/acceptance/user/progressions_spec.rb
+++ spec/acceptance/user/progressions_spec.rb
@@ -6,46 +6,56 @@ feature 'User progressions feature', %q{
   I want to view my progressions page
 } do
 
-  background do
-    @user = FactoryGirl.create(:user)
+  let(:user) { FactoryGirl.create(:user) }
 
-    @fringe       = FactoryGirl.create(:series, :name => 'Fringe', :overview => 'Supernatural stuff')
-    @boston_legal = FactoryGirl.create(:series, :name => 'Boston Legal', :overview => 'Some funny lawyers')
+  let(:fringe)       { FactoryGirl.create(:series, name: 'Fringe',       overview: 'Supernatural stuff') }
+  let(:boston_legal) { FactoryGirl.create(:series, name: 'Boston Legal', overview: 'Some funny lawyers') }
 
-    @f_ep1 = FactoryGirl.create(:episode, :series => @fringe, :name => "Fringe - Episode 1")
-    @f_ep2 = FactoryGirl.create(:episode, :series => @fringe, :name => "Fringe - Episode 2")
-    @f_ep3 = FactoryGirl.create(:episode, :series => @fringe, :name => "Fringe - Episode 3")
-    @bl_ep1 = FactoryGirl.create(:episode, :series => @boston_legal, :name => "Boston Legal - Episode 1")
+  let(:f_ep1)  { FactoryGirl.create(:episode, series: fringe,       name: 'Fringe - Episode 1',       overview: 'Overview 1') }
+  let(:f_ep2)  { FactoryGirl.create(:episode, series: fringe,       name: 'Fringe - Episode 2',       overview: 'Overview 2') }
+  let(:f_ep3)  { FactoryGirl.create(:episode, series: fringe,       name: 'Fringe - Episode 3',       overview: 'Overview 3') }
+  let(:bl_ep1) { FactoryGirl.create(:episode, series: boston_legal, name: 'Boston Legal - Episode 1', overview: 'Overview 4') }
 
-    @viewing1 = FactoryGirl.create(:viewing, :user => @user, :episode => @f_ep1, :series => @fringe, :viewed_at => 3.days.ago)
-    @viewing2 = FactoryGirl.create(:viewing, :user => @user, :episode => @f_ep2, :series => @fringe, :viewed_at => 1.days.ago)
-    @viewing3 = FactoryGirl.create(:viewing, :user => @user, :episode => @f_ep3, :series => @fringe, :viewed_at => 2.days.ago)
-    @viewing4 = FactoryGirl.create(:viewing, :user => @user, :episode => @bl_ep1, :series => @boston_legal)
-  end
+  let!(:viewing1) { FactoryGirl.create(:viewing, user: user, episode: f_ep1,  series: fringe, viewed_at: 3.days.ago) }
+  let!(:viewing2) { FactoryGirl.create(:viewing, user: user, episode: f_ep2,  series: fringe, viewed_at: 1.days.ago) }
+  let!(:viewing3) { FactoryGirl.create(:viewing, user: user, episode: f_ep3,  series: fringe, viewed_at: 2.days.ago) }
+  let!(:viewing4) { FactoryGirl.create(:viewing, user: user, episode: bl_ep1, series: boston_legal) }
 
   scenario 'Viewing my series list' do
-    fringe_last = @user.last_viewing(@fringe.id)
-    fringe_next = @user.next_viewing(@fringe.id)
-    boston_legal_last = @user.last_viewing(@boston_legal.id)
-    boston_legal_next = @user.next_viewing(@boston_legal.id)
+    fringe_last = user.last_viewing(fringe.id)
+    fringe_next = user.next_viewing(fringe.id)
+    boston_legal_last = user.last_viewing(boston_legal.id)
+    boston_legal_next = user.next_viewing(boston_legal.id)
 
-    log_in @user
+    log_in user
     visit progressions_path
 
-    # save_and_open_page
-    page.should have_content @fringe.name
-    page.should have_content @fringe.overview
-    page.should have_content fringe_last.episode.name
-    page.should have_content fringe_last.episode.overview
-    page.should have_content 'No upcoming episodes'
+    within(:xpath, "//section[@id='progression'][1]") do
+      expect(page).to have_content('Fringe')
+      expect(page).to have_content('Supernatural stuff')
+      within('#last_viewing') do
+        expect(page).to have_content('Fringe - Episode 2')
+        expect(page).to have_content('Overview 2')
+      end
+      within('#next_viewing') do
+        expect(page).to have_content('No upcoming episodes')
+      end
+    end
 
-    page.should have_content @boston_legal.name
-    page.should have_content @boston_legal.overview
-    page.should have_content 'No previously watched episodes'
-    page.should have_content boston_legal_next.episode.name
-    page.should have_content boston_legal_next.episode.overview
+    within(:xpath, "//section[@id='progression'][2]") do
+      expect(page).to have_content('Boston Legal')
+      expect(page).to have_content('Some funny lawyers')
+      within('#last_viewing') do
+        expect(page).to have_content('No previously watched episodes')
+      end
+      within('#next_viewing') do
+        expect(page).to have_content('Boston Legal - Episode 1')
+        expect(page).to have_content('Overview 4')
+      end
+    end
 
     click_button 'Watch'
-    page.should_not have_content 'No previously watched episodes'
+
+    expect(page).to_not have_content 'No previously watched episodes'
   end
 end
